49f92112c732498758d570e06e8c114af8b446bd
KarlGilden <karl.gildenhuys@gmail.com>
2025-09-07 21:54:31 +1200

    habit types

---
+++ server/src/types/habit.ts
@@ -0,0 +1,52 @@
+// ==========================
+// Habit Sheets
+// ==========================
+export interface HabitSheet {
+  id: number;
+  user_id: number;
+  name: string;
+  description: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+// ==========================
+// Habits
+// ==========================
+export type EntryType = "boolean" | "number";
+
+export interface Habit {
+  id: number;
+  habit_sheet_id: number;
+  name: string;
+  description: string;
+  question: string | null;
+  entry_type: EntryType;
+  created_at: Date;
+  updated_at: Date;
+}
+
+// ==========================
+// Numeric constraints
+// ==========================
+export type ConstraintType = "equal" | "less_than" | "more_than" | "between";
+
+export interface HabitNumericConstraint {
+  id: number;
+  habit_id: number;
+  constraint_type: ConstraintType;
+  lower_bound: number;
+  upper_bound: number | null;
+}
+
+// ==========================
+// Habit entries
+// ==========================
+export interface HabitEntry {
+  id: number;
+  habit_id: number;
+  value: number;
+  date: string;
+  created_at: Date;
+  is_pass: boolean;
+}
